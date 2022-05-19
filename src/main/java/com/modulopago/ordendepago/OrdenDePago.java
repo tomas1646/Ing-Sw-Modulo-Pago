408ea16522dd242b32a1e168f2c0d1c9a3d45b62
Ales1988 <102316409+Ales1988@users.noreply.github.com>
2022-05-20 01:31:54 +0200

    5 nuevos test para la orden de pago

--- src/main/java/com/modulopago/ordendepago/OrdenDePago.java
+++ src/main/java/com/modulopago/ordendepago/OrdenDePago.java
@@ -1,4 +1,58 @@
 package com.modulopago.ordendepago;
 
 public class OrdenDePago {
+
+    private String numeroOrdenDePago;
+    private String numeroVenta;
+    private String numeroCliente;
+    private String numeroEnvio;
+    private float monto;
+
+    public OrdenDePago(String numeroOrdenDePago, String numeroVenta, String numeroCliente, String numeroEnvio, float monto) {
+        this.numeroOrdenDePago = numeroOrdenDePago;
+        this.numeroVenta = numeroVenta;
+        this.numeroCliente = numeroCliente;
+        this.numeroEnvio = numeroEnvio;
+        this.monto = monto;
+    }
+
+    public String getNumeroOrdenDePago() {
+        return numeroOrdenDePago;
+    }
+
+    public void setNumeroOrdenDePago(String numeroOrdenDePago) {
+        this.numeroOrdenDePago = numeroOrdenDePago;
+    }
+
+    public String getNumeroVenta() {
+        return numeroVenta;
+    }
+
+    public void setNumeroVenta(String numeroVenta) {
+        this.numeroVenta = numeroVenta;
+    }
+
+    public String getNumeroCliente() {
+        return numeroCliente;
+    }
+
+    public void setNumeroCliente(String numeroCliente) {
+        this.numeroCliente = numeroCliente;
+    }
+
+    public String getNumeroEnvio() {
+        return numeroEnvio;
+    }
+
+    public void setNumeroEnvio(String numeroEnvio) {
+        this.numeroEnvio = numeroEnvio;
+    }
+
+    public float getMonto() {
+        return monto;
+    }
+
+    public void setMonto(float monto) {
+        this.monto = monto;
+    }
 }
