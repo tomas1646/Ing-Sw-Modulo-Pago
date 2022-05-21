68c31f60f6fd8878f00e8cbf3227c7648ff739ee
florhemsy <florhemsy@gmail.com>
2022-05-21 17:52:26 -0300

    formateo codigo

--- src/test/java/com/modulopago/ordendepago/OrdenDePagoTest.java
+++ src/test/java/com/modulopago/ordendepago/OrdenDePagoTest.java
@@ -1,36 +1,37 @@
 package com.modulopago.ordendepago;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class OrdenDePagoTest {
 
     @Test
-    public void testGetNumeroOrdenDePago(){
-
+    public void testGetNumeroOrdenDePago() {
         OrdenDePago ordenDePago = new OrdenDePago("PA93003", "VE89344", "CL40123", "EN6793", 560020);
         assertEquals("PA93003", ordenDePago.getNumeroOrdenDePago());
     }
 
     @Test
-    public void testGetNumeroVenta(){
+    public void testGetNumeroVenta() {
         OrdenDePago ordenDePago = new OrdenDePago("PA93003", "VE89344", "CL40123", "EN6793", 560020);
         assertEquals("VE89344", ordenDePago.getNumeroVenta());
     }
 
     @Test
-    public void testGetNumeroCliente(){
+    public void testGetNumeroCliente() {
         OrdenDePago ordenDePago = new OrdenDePago("PA93003", "VE89344", "CL40123", "EN6793", 560020);
         assertEquals("CL40123", ordenDePago.getNumeroCliente());
     }
 
     @Test
-    public void testGetNumeroEnvio(){
+    public void testGetNumeroEnvio() {
         OrdenDePago ordenDePago = new OrdenDePago("PA93003", "VE89344", "CL40123", "EN6793", 560020);
         assertEquals("EN6793", ordenDePago.getNumeroEnvio());
     }
 
     @Test
-    public void testGetMonto(){
+    public void testGetMonto() {
         OrdenDePago ordenDePago = new OrdenDePago("PA93003", "VE89344", "CL40123", "EN6793", 560020);
         assertEquals(560020, ordenDePago.getMonto());
     }
