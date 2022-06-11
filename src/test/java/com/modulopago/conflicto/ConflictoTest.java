f997e118ed908601f126f60d840b8f61fd9f4c66
Tomas <tomasespin@outlook.com>
2022-06-11 16:21:42 -0300

    Fix tests scope

--- src/test/java/com/modulopago/conflicto/ConflictoTest.java
+++ src/test/java/com/modulopago/conflicto/ConflictoTest.java
@@ -8,7 +8,7 @@ import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ConflictoTest {
+public class ConflictoTest {
 
     @Test
     public void testSetDescripcion() {
